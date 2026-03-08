92e78bdc07ec209e9220d14d9c5749af2fa0e02c
BytesOfStrength <213146959+BytesOfStrength@users.noreply.github.com>
2026-03-07 22:20:39 -0500

    week-01-main-component: Initialize routing and App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import {Routes, Route} from "react-router";
+
+
+import Navbar from "./shared/Navbar";
+import Layout from "./shared/Layout";
 import './App.css';
+
 
 function App() {
   return (
-    <div>
-      <h1>Dessert Lab</h1>
+    <div className="app-wrapper">
+      <Navbar />
+      <Layout />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home />
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <DessertMaker />
+            }
+          />
+          <Route
+           path ="*"
+           element ={
+            <NotFound />
+           }
+          />
+        </Routes>
+      </Layout>
     </div>
-  );
+  )
 }
 
 export default App;
